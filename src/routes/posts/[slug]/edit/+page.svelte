885abafc0a377cddf1669a66f568ae2aa65180a2
tinytinytinytiny <tinynametaken@yahoo.com>
2023-01-27 13:46:13 -0500

    fix edit

--- src/routes/posts/[slug]/edit/+page.svelte
+++ src/routes/posts/[slug]/edit/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { onMount } from 'svelte';
 	import TextBox from '$lib/components/TextBox.svelte';
+	import TextArea from '$lib/components/TextArea.svelte';
 
 	onMount(() => {
 		document.querySelector('textarea').focus();
@@ -11,7 +12,9 @@
 </script>
 
 <form id="edit" method="POST" action={`/posts/${data.slug}?/edit`}>
-	<TextBox id="post" name="post" label="Edit Message" value={data.text} />
+	<TextBox>
+		<TextArea id="post" name="post" label="Edit Message" value={data.text} />
+	</TextBox>
 	<input type="hidden" name="id" value={data.id} />
 </form>
 <footer>
