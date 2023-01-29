edc5beeb8058984b98960aa8472075b2128990a4
tinytinytinytiny <tinynametaken@yahoo.com>
2023-01-29 15:51:01 -0500

    cleanup

--- src/lib/components/InputForm.svelte
+++ src/lib/components/InputForm.svelte
@@ -1,5 +1,4 @@
 <script>
-	import { onMount } from 'svelte';
 	import TextBox from '$lib/components/TextBox.svelte';
 	import TextArea from '$lib/components/TextArea.svelte';
 	import Button from '$lib/components/Button.svelte';
@@ -7,29 +6,15 @@
 	export let action = null;
 	export let placeholder;
 
-	// let button;
 	let value = '';
-
-	// onMount(handleInput);
-
-	// function handleInput() {
-	// 	button.disabled = Boolean(!value.replace(/\s/g, ''));
-	// }
 </script>
 
 <form method="POST" {action}>
 	<slot />
 	<TextBox>
 		<TextArea id="compose-post" name="post" label="Message" {placeholder} bind:value />
-		<div class="button-wrapper">
+		<div class="self-start p-xs-fixed">
 			<Button type="submit" disabled={Boolean(!value.replace(/\s/g, ''))}>Post</Button>
 		</div>
 	</TextBox>
 </form>
-
-<style>
-	.button-wrapper {
-		align-self: flex-start;
-		padding: theme('spacing.xs-fixed');
-	}
-</style>
