a6df2dabb1f06d2cf13d7c7ef3a3d1c30ec68353
tinytinytinytiny <tinynametaken@yahoo.com>
2023-01-29 18:56:07 -0500

    redirect homepage

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { page } from '$app/stores';
+	import Main from '$lib/components/Main.svelte';
+</script>
+
+<Main>
+	<h1>{$page.status}</h1>
+	<p>
+		{$page.error.message}
+	</p>
+</Main>
