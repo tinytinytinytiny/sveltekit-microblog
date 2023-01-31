fc371634a3be748498129340f0c1a63ccc4517bf
tinytinytiny <105854913+tinytinytinytiny@users.noreply.github.com>
2023-01-31 14:34:50 +0000

    modify textbox component structure part 2

--- src/lib/components/InputForm.svelte
+++ src/lib/components/InputForm.svelte
@@ -11,10 +11,18 @@
 
 <form method="POST" {action}>
 	<slot />
-	<TextBox label="Message" id="compose-post">
-		<TextArea name="post" required {placeholder} bind:value />
+	<label class="visually-hidden" for="compose-message">Message</label>
+	<div class="input flex items-center">
+		<TextArea id="compose-message" name="post" required {placeholder} bind:value />
 		<div class="self-start p-xs-fixed">
 			<Button type="submit" disabled={Boolean(!value.replace(/\s/g, ''))}>Post</Button>
 		</div>
-	</TextBox>
+	</div>
 </form>
+
+<style>
+	input:global(:has(textarea:focus)) {
+		outline: 2px dashed var(--color-focus-ring);
+		outline-offset: 3px;
+	}
+</style>
