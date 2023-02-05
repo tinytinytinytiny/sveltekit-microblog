7189a17d627370c5297cc746fe5671a05d438082
tinytinytinytiny <tinynametaken@yahoo.com>
2023-02-05 14:27:48 -0500

    fix pagination array groups (probably)

--- src/routes/posts/Pagination.svelte
+++ src/routes/posts/Pagination.svelte
@@ -3,11 +3,12 @@
 	export let page;
 
 	const MAX_PAGES = 5;
+	const PAGE_GROUPS = Math.ceil(Number(count) / (MAX_PAGES - 1));
 	let pages;
 	let visiblePages;
 
 	$: {
-		pages = [...Array(Math.ceil(Number(count) / MAX_PAGES) + 1)].map((_, group) =>
+		pages = [...Array(PAGE_GROUPS)].map((_, group) =>
 			[...Array(MAX_PAGES)]
 				.map(() => group * MAX_PAGES)
 				.map((x, i) => x + i + 1)
