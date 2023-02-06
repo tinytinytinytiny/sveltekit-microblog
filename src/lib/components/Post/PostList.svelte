c65d4f03cedd55edd307a79019de10f8ec4caf9d
tinytinytiny <105854913+tinytinytinytiny@users.noreply.github.com>
2023-02-06 20:53:39 +0000

    remove thread class

--- src/lib/components/Post/PostList.svelte
+++ src/lib/components/Post/PostList.svelte
@@ -1,9 +1,17 @@
 <!-- svelte-ignore a11y-no-redundant-roles -->
-<ul class="post-list thread stack stack-space-m" role="list">
+<ul class="post-list stack stack-space-m" role="list">
 	<slot />
 </ul>
 
 <style>
+	:global(.post-list .post-list) {
+		margin-inline-start: theme('spacing.m-l-fixed');
+	}
+
+	:global(.post-list :where(li > .post-list)) {
+		margin-block-start: var(--stack-space, 1em);
+	}
+
 	:global(.post-list > :where(li + li)),
 	:global(.post-list :where(li > .post-list)) {
 		border-block-start: 1px solid var(--color-separator);
