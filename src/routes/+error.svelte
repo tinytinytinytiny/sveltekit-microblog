980633a340c1274a1b0b718525eb7ab10ce3a817
tinytinytinytiny <tinynametaken@yahoo.com>
2023-02-07 05:35:54 -0500

    add 404 error

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -4,8 +4,15 @@
 </script>
 
 <Main>
-	<h1>{$page.status}</h1>
-	<p>
-		{$page.error.message}
-	</p>
+	<div class="flex flex-col-reverse">
+		<header class="mbs-l-xl stack copy">
+			<h1>{$page.status}</h1>
+			<p>
+				{$page.error.message}
+			</p>
+		</header>
+		<nav>
+			<a href="/posts#posts">Back to blog</a>
+		</nav>
+	</div>
 </Main>
--- src/routes/posts/[id]/+layout.server.js
+++ src/routes/posts/[id]/+layout.server.js
@@ -1,3 +1,12 @@
 import { getPost } from '$lib/server';
+import { error } from '@sveltejs/kit';
 
-export const load = async ({ params }) => getPost(params.id);
+export const load = async ({ params }) => {
+	const post = await getPost(params.id);
+
+	if (!post) throw error(404, {
+		message: 'Not found'
+	});
+
+	return post;
+};
