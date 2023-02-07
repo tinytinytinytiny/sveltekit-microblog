9599c1cd2e30fe077e7e209166238ba9dd3461db
tinytinytinytiny <tinynametaken@yahoo.com>
2023-02-06 20:27:15 -0500

    remove spam

--- src/routes/posts/SiteHeader.svelte
+++ src/routes/posts/SiteHeader.svelte
@@ -1,10 +1,16 @@
-<header class="full-bleed plb-l-xl-fixed mbe-l-xl text-center">
+<header class="full-bleed">
 	<h1>
 		<a class="text-secondary no-underline" href="/posts">Microblog</a>
 	</h1>
 </header>
 
 <style>
+	header {
+		margin-block-end: theme('spacing.l-xl');
+		padding-block: theme('spacing.l-xl-fixed');
+		text-align: center;
+	}
+
 	header h1 {
 		font-size: max(3.14rem, 1rem + 16vw);
 		letter-spacing: -0.0333em;
